e75be508576f62ec8837036ee9e49246e6074531
unknown <yashagarwal918@gmail.com>
2020-04-10 11:30:06 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,10 +2,25 @@ const User=require("../models/users.js");
 
 module.exports.profile=function(req,res)
 {
-    return res.render('users_profile.ejs',
+    if(req.cookies.user_id)
     {
-        title : "Codeial Profile Page"
-    });
+        User.findById(req.cookies.user_id,function(err,user)
+        {
+            if(user)
+            {
+                return res.render('users_profile.ejs',{
+                    title : "Users Profile",
+                    user : user
+                });
+            }
+            res.clearCookie("user_id");
+            return(res.redirect("/users/sign-in"));
+        });
+    }
+    else
+    {
+        return(res.redirect("/users/sign-in"));
+    }
 }
 
 // render the sign in page
@@ -17,7 +32,25 @@ module.exports.signUp=function(req,res)
 // render the sign up page
 module.exports.signIn=function(req,res)
 {
-    return(res.render('user_sign_in.ejs',{title:"Codeial | Sign In"}));
+    if(req.cookies.user_id)
+    {
+        User.findById(req.cookies.user_id,function(err,user)
+        {
+            if(user)
+            {
+                return res.redirect("/users/profile");
+            }
+            else
+            {
+                res.clearCookie("user_id");
+                return(res.render('user_sign_in.ejs',{title:"Codeial | Sign In"}));
+            }
+        });
+    }
+    else
+    {
+        return(res.render('user_sign_in.ejs',{title:"Codeial | Sign In"}));
+    }
 };
 
 module.exports.create=function(req,res)
@@ -46,5 +79,29 @@ module.exports.create=function(req,res)
 
 module.exports.createSession=function(req,res)
 {
+    // steps to authoenticate
+    // find the user
+    User.findOne({email:req.body.email},function(err,user)
+    {
+        if(err){console.log("error in finding user in signing in");return;}
+        // handle user found
+        if(user)
+        {
+            //handle passwords mismatching
+            if(user.password!=req.body.password)
+            {
+                return(res.redirect("back"));
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            // console.log(req.cookie);
+            return(res.redirect("./profile"));
+        }
+        // handle user not found
+        else
+        {
+            return(res.redirect("back"));
+        }
 
+    });
 };
