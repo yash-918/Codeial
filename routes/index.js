e22836551bab3a6b12fc2c7f8fb64b4471281e01
unknown <yashagarwal918@gmail.com>
2020-04-17 17:56:07 +0530

    setup the directories for server-side-APIs and successfully made the 1st api call

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,13 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
+// routes for api call
+router.use("/api",require('./api'));
+
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
+
+
 
 
 module.exports = router;
